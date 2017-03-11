3f2d77603e2795a94eee2971ee44136e10f03ead
alexzolkin19 <alexzolkin19@gmail.com>
2017-03-11 16:42:57 +0300

    factorial is done

---
+++ src/secondTask/Factorial.java
@@ -0,0 +1,12 @@
+package secondTask;
+
+/**
+ * Created by Алексей on 11.03.2017.
+ */
+public class Factorial {
+    public static int factorial(int n){
+        if(n == 0 || n == 1)
+            return 1;
+        return n * factorial(n-1);
+    }
+}
