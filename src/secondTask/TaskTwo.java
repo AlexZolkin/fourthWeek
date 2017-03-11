6574cbd77f76603db0e620580a4e4cf2399379b6
alexzolkin19 <alexzolkin19@gmail.com>
2017-03-11 20:51:57 +0300

    parenBit is done

--- src/secondTask/TaskTwo.java
+++ src/secondTask/TaskTwo.java
@@ -205,6 +205,21 @@ public class TaskTwo {
     }
 
     /*
+    * get substring between
+    * "(" and ")"
+    * */
+    public String parenBit(String str){
+        if(str.length() == 0)
+            return str;
+        if(str.charAt(0) == '(' && str.charAt(str.length() - 1) == ')'){
+            return str;
+        }else if(str.charAt(0) == '('){
+            return parenBit(str.substring(0,str.length()-1));
+        }else
+            return parenBit(str.substring(1));
+    }
+
+    /*
     * Count number of sub strings
     * in string given
     * */
