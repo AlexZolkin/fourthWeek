65718a8eea90b61ab2c1542cdc204ac956c0f9c3
alexzolkin19 <alexzolkin19@gmail.com>
2017-03-11 19:27:57 +0300

    count8 is done

--- src/secondTask/TaskTwo.java
+++ src/secondTask/TaskTwo.java
@@ -56,6 +56,20 @@ public class TaskTwo {
     }
 
     /*
+    * Count how many digits "8"
+    * are there in number given
+    * */
+    public int count8(int n){
+        if(n == 0)
+            return 0;
+        if(n % 100 == 88)
+            return 2 + count8(n / 10);
+        if(n % 10 == 8)
+            return 1 + count8(n / 10);
+        return count8(n / 10);
+    }
+
+    /*
     * Count number of occupancy's of "x"
     * in string given
     * */
