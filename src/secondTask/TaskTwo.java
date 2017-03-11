4c7b0dee3dbce99361669b0c4c5fae96fdd35fc8
alexzolkin19 <alexzolkin19@gmail.com>
2017-03-11 22:01:05 +0300

    strDist is done all tasks from Recursion-1 are done

--- src/secondTask/TaskTwo.java
+++ src/secondTask/TaskTwo.java
@@ -342,7 +342,24 @@ public class TaskTwo {
             return strCopies(str.substring(str.indexOf(sub) + 1), sub, n - 1);
         }else
             return true;
+    }
 
+    /*
+    * Find the largest string between substrings given
+    * */
+    public int strDist(String str, String sub){
+        int strLen = str.length();
+        int subLen = sub.length();
+
+        if(strLen < subLen)
+            return 0;
+
+        if(str.substring(0, subLen).equals(sub)){
+            if(str.substring(strLen - subLen, strLen).equals(sub))
+                return strLen;
+            return strDist(str.substring(0, strLen - 1), sub);
+        }
+        return strDist(str.substring(1), sub);
     }
 
 }
