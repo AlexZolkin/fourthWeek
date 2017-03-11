1928d2ebbfe92550e43853fa4102c7245d384c87
alexzolkin19 <alexzolkin19@gmail.com>
2017-03-11 17:31:31 +0300

    array11 is done

--- src/secondTask/TaskTwo.java
+++ src/secondTask/TaskTwo.java
@@ -53,4 +53,13 @@ public class TaskTwo {
         return str.equals(res) ? res : changePi(res);
     }
 
+    /*
+    * Counts number of "11" in array
+    * */
+    public int array11(int[] nums, int index){
+        if(index == nums.length)
+            return 0;
+        return nums[index] == 11 ? 1 + array11(nums, index + 1) : array11(nums, index + 1);
+    }
+
 }
