4afe48e6e4b24d5cb0e9343ec2c1b2904f0c87dc
alexzolkin19 <alexzolkin19@gmail.com>
2017-03-11 16:55:13 +0300

    tasks in 1 class

--- src/secondTask/TaskTwo.java
+++ src/secondTask/TaskTwo.java
@@ -3,7 +3,16 @@ package secondTask;
 /**
  * Created by Алексей on 11.03.2017.
  */
-public class BunnyEars2 {
+public class TaskTwo {
+
+    /*
+    * Calculating factorial
+    * */
+    public int factorial(int n){
+        if(n == 0 || n == 1)
+            return 1;
+        return n * factorial(n-1);
+    }
 
     /*
     * even -> +=3
@@ -14,5 +23,4 @@ public class BunnyEars2 {
         if(bunnies == 0) return 0;
         return bunnies % 2 == 0 ? 3 + bunnyEars2(bunnies - 1) : 2 + bunnyEars2(bunnies - 1);
     }
-
 }
