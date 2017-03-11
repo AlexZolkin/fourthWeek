72016dbaddd0370d91c674e09e32bcc8844cb69a
alexzolkin19 <alexzolkin19@gmail.com>
2017-03-11 21:47:02 +0300

    stringClean is done

--- src/secondTask/TaskTwo.java
+++ src/secondTask/TaskTwo.java
@@ -235,6 +235,20 @@ public class TaskTwo {
     }
 
     /*
+    * reducing multiple same chars to the one
+    * in the string given
+    * */
+    public String stringClean(String str){
+        if(str.length() == 1)
+            return str;
+
+        if(str.charAt(0) == str.charAt(1)){
+            return stringClean(str.substring(1));
+        }else
+            return str.charAt(0) + stringClean(str.substring(1));
+    }
+
+    /*
     * count number of "hi"
     * */
     public int countHi(String str){
