386dabe010036a8ce6dc6299cd51ae6da0b5ebf9
alexzolkin19 <alexzolkin19@gmail.com>
2017-03-11 21:39:41 +0300

    countPairs is done

--- src/secondTask/TaskTwo.java
+++ src/secondTask/TaskTwo.java
@@ -197,6 +197,19 @@ public class TaskTwo {
     }
 
     /*
+    * Count number of letter pairs
+    * separated by third one
+    * */
+    public int countPairs(String str){
+        if(str.length() < 3)
+            return 0;
+        if(str.charAt(0) == str.charAt(2)){
+            return 1 + countPairs(str.substring(1));
+        }
+        return countPairs(str.substring(1));
+    }
+
+    /*
     * Count number of "abc" and "aba"
     * substrings in string given
     * */
