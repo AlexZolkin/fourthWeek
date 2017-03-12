018414c0e035287a015dfd0fbe13f8165eefccdc
alexzolkin19 <alexzolkin19@gmail.com>
2017-03-12 23:16:41 +0300

    second is done just found some clues in the net fo those that's really mind blowing staff trust me, fuck recursion, all the difficult staff can be realized easier that's horrible

--- src/thirdTask/Recursions.java
+++ src/thirdTask/Recursions.java
@@ -18,5 +18,24 @@ public class Recursions {
         return groupSum(start+1, nums, target);
     }
 
+    /*
+    * equal to groupSum
+    * all the 5 multiples must be included
+    * */
+    public boolean groupSum5(int start, int[] nums, int target){
+        if(start >= nums.length){
+            if(target == 0)
+                return true;
+            return false;
+        }
+        if(nums[start] % 5 == 0){
+            if(start < nums.length - 1 && nums[start+1] == 1)
+                return groupSum5(start + 2, nums, target - nums[start]);
+            return groupSum5(start + 1, nums, target - nums[start]);
+        }
+        if(groupSum5(start+1,nums,target - nums[start]))
+            return true;
+        return groupSum5(start+1, nums, target);
+    }
 
 }
